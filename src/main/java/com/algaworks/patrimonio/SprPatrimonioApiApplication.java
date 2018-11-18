d49ca01e16927b055b5150503d83c89a086c3c07
Helio Diniz <helio.diniz74@gmail.com>
2018-11-18 16:37:04 -0200

    primeiro envio

---
+++ src/main/java/com/algaworks/patrimonio/SprPatrimonioApiApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.patrimonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprPatrimonioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprPatrimonioApiApplication.class, args);
+	}
+}
